052442c91e02fe5eb423f8166cb4d0b37726049c
John Cassidy <jpcassidy@outlook.com>
2023-03-12 09:55:00 -0400

    comparing performance of different grpc client types

---
+++ ApiGateway/GrpcPerformanceClient.cs
@@ -0,0 +1,36 @@
+﻿using Grpc.Net.Client;
+using Performance;
+using Monitor = Performance.Monitor;
+
+namespace ApiGateway {
+    public interface IGrpcPerformanceClient {
+        Task<ResponseModel.PerformanceStatusModel> GetPerformanceStatus(string clientName);
+    }
+
+    internal class GrpcPerformanceClient : IGrpcPerformanceClient, IDisposable {
+        private readonly GrpcChannel channel;
+
+        public GrpcPerformanceClient(string serverUrl) {
+            channel = GrpcChannel.ForAddress(serverUrl);
+        }
+
+        public async Task<ResponseModel.PerformanceStatusModel> GetPerformanceStatus(string clientName) {
+            var client = new Monitor.MonitorClient(channel);
+
+            var response = await client.GetPerformanceAsync(new PerformanceStatusRequest {
+                ClientName = clientName
+            });
+
+            return new ResponseModel.PerformanceStatusModel {
+                CpuPercentageUsage = response.CpuPercentageUsage,
+                MemoryUsage = response.MemoryUsage,
+                ProcessesRunning = response.ProcessesRunning,
+                ActiveConnections = response.ActiveConnections
+            };
+        }
+
+        public void Dispose() {
+            channel.Dispose();
+        }
+    }
+}
--- PerformanceService/Program.cs
+++ PerformanceService/Program.cs
@@ -1,4 +1,5 @@
-using PerformaceService.Services;
+
+using PerformanceService;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +12,7 @@ builder.Services.AddGrpc();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+app.MapGrpcService<PerformanceMonitor>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
