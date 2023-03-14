bb8512eafa5233bf2c100e28af75072a67f108a6
John Cassidy <jpcassidy@outlook.com>
2023-03-14 11:01:06 -0400

    enable grpc channel - EnableMultipleHttp2Connections

---
+++ ApiGateway/Controllers/ConcurrencyController.cs
@@ -0,0 +1,67 @@
+﻿using Grpc.Net.Client;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Performance;
+using System.Diagnostics;
+using Monitor = Performance.Monitor;
+
+namespace ApiGateway.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConcurrencyController : ControllerBase {
+        private readonly string serverUrl;
+
+        public ConcurrencyController(IConfiguration configuration) {
+            serverUrl = configuration["ServerUrl"];
+        }
+
+
+        [HttpGet("single-connection/{count}")]
+        public ResponseModel GetDataFromSingleConnection(int count) {
+            using var channel = GrpcChannel.ForAddress(serverUrl);
+            var stopWatch = Stopwatch.StartNew();
+
+            var response = new ResponseModel();
+
+            var concurrentJobs = new List<Task>();
+            for (var i = 0; i < count; i++) {
+                var client = new Monitor.MonitorClient(channel);
+                concurrentJobs.Add(Task.Run(() => {
+                    client.GetPerformance(new PerformanceStatusRequest { ClientName = $"client {i + 1}" });
+                }));
+            }
+
+            Task.WaitAll(concurrentJobs.ToArray());
+
+            response.RequestProcessingTime = stopWatch.ElapsedMilliseconds;
+
+            return response;
+        }
+
+        [HttpGet("multiple-connections/{count}")]
+        public ResponseModel GetDataFromMultipleConnections(int count) {
+            using var channel = GrpcChannel.ForAddress(serverUrl, new GrpcChannelOptions {
+                HttpHandler = new SocketsHttpHandler {
+                    EnableMultipleHttp2Connections = true,
+                }
+            });
+            var stopWatch = Stopwatch.StartNew();
+
+            var response = new ResponseModel();
+
+            var concurrentJobs = new List<Task>();
+            for (var i = 0; i < count; i++) {
+                concurrentJobs.Add(Task.Run(() => {
+                    var client = new Monitor.MonitorClient(channel);
+                    client.GetPerformance(new PerformanceStatusRequest { ClientName = $"client {i + 1}" });
+                }));
+            }
+
+            Task.WaitAll(concurrentJobs.ToArray());
+
+            response.RequestProcessingTime = stopWatch.ElapsedMilliseconds;
+
+            return response;
+        }
+    }
+}
